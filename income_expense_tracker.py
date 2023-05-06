1bbdc8cdb99e8e9a07faf808abd87e10d2c5e49c
Matthew Patterson <matt.patterson1738@hotmail.com>
2023-05-07 09:31:09 +1000

    define function to add account

--- income_expense_tracker.py
+++ income_expense_tracker.py
@@ -1,3 +1,11 @@
 import pandas as pd
 from pprint import pprint
 from datetime import datetime
+
+class income_expense_tracker:
+    def __init__(self):
+        self.accounts = {}
+
+    def add_account(self, name, balance=0):
+        self.accounts[name] = balance
+    
