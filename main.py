404363fbd54811447f2fade01782d9fa3f9e5d9f
Matthew Patterson <matt.patterson1738@hotmail.com>
2023-05-07 11:52:35 +1000

    import click module and add some input options for the main file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from budget import budget
 from datetime import datetime
+from click import click
 
 #Create instance for the budget
 my_budget = budget()
+
+options = ['Add an account', 'Add a transaction', 'View transactions or accounts', 'Set a budget limit', 'View budget limits', 'View the remaining budget for the month']
