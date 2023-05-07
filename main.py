297e5baf3613db9e044c1bc18d0f6b51b5d8c029
Matthew Patterson <matt.patterson1738@hotmail.com>
2023-05-07 15:22:23 +1000

    Added the ability to add an account in the main file

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from budget import budget
 from datetime import datetime
-from click import click
+import click
 
 #Create instance for the budget
 my_budget = budget()
 
-options = ['Add an account', 'Add a transaction', 'View transactions or accounts', 'Set a budget limit', 'View budget limits', 'View the remaining budget for the month']
+feature_options = ['Add an account', 'Add a transaction', 'View transactions or accounts', 'Set a budget limit', 'View budget limits', 'View the remaining budget for the month']
+feature_choice = click.prompt('What would you like to do?: ', type=click.Choice(feature_options))
+
+if feature_choice == 'Add an account':
+    add_account = True
+    while add_account:
+        account_name = click.prompt('What is the name of the account?', type=str)
+        budget.add_account(account_name)
+        add_account_input = click.prompt('Would you like to add another account? (yes or no): ', type=str).lower()
+        #add error handling for the input
+        if add_account_input == 'no':
+            add_account = False
+        
+
